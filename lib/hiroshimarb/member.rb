6762c8a9b019e47c393bf6ee67c250d43f718740
Tomohiko Himura <eiel.hal@gmail.com>
2012-09-04 23:24:40 +0900

    Member#to_sを実装

--- lib/hiroshimarb/member.rb
+++ lib/hiroshimarb/member.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 module Hiroshimarb
   # メンバーを表現すくクラス
   class Member
@@ -6,7 +7,17 @@ module Hiroshimarb
       end
     end
 
+    attr_accessor :name, :github, :website, :twitter, :profile
+
     def to_s
+      <<EOD
+   name       #{name}
+   github id  #{github}
+   twitter id #{twitter}
+   website    #{website}
+   profile
+     #{profile}
+EOD
     end
   end
 end
--- spec/hiroshimarb/member_spec.rb
+++ spec/hiroshimarb/member_spec.rb
@@ -1,11 +1,36 @@
+# -*- coding: utf-8 -*-
 require 'hiroshimarb/member'
 
 describe Hiroshimarb::Member do
+  before :each do
+    include Hiroshimarb
+  end
+
   describe '.all' do
     it
   end
 
   describe '#to_s' do
-    it
+    subject { member.to_s }
+
+    let(:member) do
+      m = Hiroshimarb::Member.new
+      m.name = 'ruby Hiroshima'
+      m.github = 'hiroshimarb'
+      m.website = 'http://hiroshimarb.github.com/'
+      m.profile = 'Hiroshima.rb'
+      m
+    end
+
+    it do
+      should eq(<<EOD)
+   name       ruby Hiroshima
+   github id  hiroshimarb
+   twitter id 
+   website    http://hiroshimarb.github.com/
+   profile
+     Hiroshima.rb
+EOD
+    end
   end
 end
