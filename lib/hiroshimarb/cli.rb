f25922627b6dcb329d07496c2ba83e6bd9d087a8
北䑓如法 <algebraicallyClosedField@gmail.com>
2012-10-19 17:19:11 +0900

    open コマンドの実装をリファクタリングした。open.rb を新規作成。

--- lib/hiroshimarb/cli.rb
+++ lib/hiroshimarb/cli.rb
@@ -9,20 +9,7 @@ module Hiroshimarb
   module CLI
     # Hiroshim.rbのウェブサイトをブラウザで開く
     def open(key = nil)
-      key ||= 'hiroshimarb'
-      url = case key
-            when 'hiroshimarb'
-              'http://hiroshimarb.github.com'
-            when 'oc-h'
-              'http://oc-h.jp'
-            when 'ipad'
-              URI.encode 'http://kita.dyndns.org/wiki/?広島弁吹き替えシリーズ'
-            when 'city'
-              'http://www.city.hiroshima.lg.jp'
-            when 'pref'
-              'http://www.pref.hiroshima.lg.jp'
-            end
-      Launchy.open url
+      Open.open(key)
     end
 
     # Hiroshim.rbの情報を標準出力へ表示
