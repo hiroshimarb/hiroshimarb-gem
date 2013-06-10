12c74d755f0f91e71e733f9155c3ff6e274d8e92
Tomohiko Himura <eiel.hal@gmail.com>
2013-06-11 00:10:23 +0900

    失敗するスペックを修正

--- spec/hiroshimarb/event_spec.rb
+++ spec/hiroshimarb/event_spec.rb
@@ -11,10 +11,10 @@ module Hiroshimarb
 
     describe '.recent' do
       subject { Event.recent }
-      its(:title) { should eq('広島Ruby勉強会 #031') }
-      it '日付は2012年 4月6日' do
+      its(:title) { should eq('広島Ruby勉強会 #032') }
+      it '日付は2012年 6月6日' do
         date = subject.start_datetime
-        expect(date.strftime('%Y-%m-%d')).to eq('2013-04-06')
+        expect(date.strftime('%Y-%m-%d')).to eq('2013-06-06')
       end
     end
   end
