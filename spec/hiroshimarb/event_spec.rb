f891d5a2768ea8b15b7c341923f59e05dafe4257
Tomohiko Himura <eiel.hal@gmail.com>
2013-08-21 18:16:23 +0900

    34回の情報を追加

--- spec/hiroshimarb/event_spec.rb
+++ spec/hiroshimarb/event_spec.rb
@@ -6,15 +6,15 @@ module Hiroshimarb
   describe Event do
     describe '.all' do
       subject { Event.all }
-      it { should have(5).items }
+      it { should have(8).items }
     end
 
     describe '.recent' do
       subject { Event.recent }
-      its(:title) { should eq('広島Ruby勉強会 #032') }
-      it '日付は2012年 6月6日' do
+      its(:title) { should eq('広島Ruby勉強会 #034') }
+      it '日付は2013年 9月7日' do
         date = subject.start_datetime
-        expect(date.strftime('%Y-%m-%d')).to eq('2013-06-06')
+        expect(date.strftime('%Y-%m-%d')).to eq('2013-09-07')
       end
     end
   end
