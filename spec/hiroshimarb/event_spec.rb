39c26b16c7b7c3e76dffda11e957fcc85bf46e6c
Tomohiko Himura <eiel.hal@gmail.com>
2013-09-08 10:49:17 +0900

    第二回 中国地方DB勉強会 をイベントに追加

--- spec/hiroshimarb/event_spec.rb
+++ spec/hiroshimarb/event_spec.rb
@@ -6,15 +6,15 @@ module Hiroshimarb
   describe Event do
     describe '.all' do
       subject { Event.all }
-      it { should have(8).items }
+      it { should have(9).items }
     end
 
     describe '.recent' do
       subject { Event.recent }
-      its(:title) { should eq('広島Ruby勉強会 #034') }
-      it '日付は2013年 9月7日' do
+      its(:title) { should eq('第二回　中国地方DB勉強会') }
+      it '日付は2013年 10月5日' do
         date = subject.start_datetime
-        expect(date.strftime('%Y-%m-%d')).to eq('2013-09-07')
+        expect(date.strftime('%Y-%m-%d')).to eq('2013-10-05')
       end
     end
   end
